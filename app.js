dda91f2b41443660644a07809c9c75949f2f4083
bardanad <nadavbardavid@gmail.com>
2019-06-17 18:29:09 +0300

    Changes req.body -> req.query

--- app.js
+++ app.js
@@ -30,8 +30,9 @@ app.use("/user", (req, res, next) => {
 
 
 app.post("/login", async function (req, res) {
-    let Username = req.body.Username;
-    let Password = req.body.Password;
+    console.log(req);
+    let Username = req.query.Username;
+    let Password = req.query.Password;
     var token = await user_module.login(Username, Password);
     if (token instanceof Error) {
         console.log(token);
@@ -43,15 +44,15 @@ app.post("/login", async function (req, res) {
 });
 
 app.post("/register", async function (req, res) {
-    let Username = req.body.Username;
-    let Password = req.body.Password;
-    let Firstname = req.body.Firstname;
-    let Lastname = req.body.Lastname;
-    let City = req.body.City;
-    let Country = req.body.CountryID;
-    let Email = req.body.Email;
-    let Interests = req.body.Interests;
-    let QA = req.body.QA;
+    let Username = req.query.Username;
+    let Password = req.query.Password;
+    let Firstname = req.query.Firstname;
+    let Lastname = req.query.Lastname;
+    let City = req.query.City;
+    let Country = req.query.CountryID;
+    let Email = req.query.Email;
+    let Interests = req.query.Interests;
+    let QA = req.query.QA;
     var result = await user_module.register(Username, Password, Firstname, Lastname, City, Country, Email, Interests, QA);
     if (result instanceof Error) {
         console.log(result);
@@ -63,14 +64,14 @@ app.post("/register", async function (req, res) {
 
 app.post("/user/updateUser", async function (req, res) {
     let Username = req.decoded.username;
-    let Password = req.body.Password;
-    let Firstname = req.body.Firstname;
-    let Lastname = req.body.Lastname;
-    let City = req.body.City;
-    let Country = req.body.CountryID;
-    let Email = req.body.Email;
-    let Interests = req.body.Interests;
-    let QA = req.body.QA;
+    let Password = req.query.Password;
+    let Firstname = req.query.Firstname;
+    let Lastname = req.query.Lastname;
+    let City = req.query.City;
+    let Country = req.query.CountryID;
+    let Email = req.query.Email;
+    let Interests = req.query.Interests;
+    let QA = req.query.QA;
     var result = await user_module.updateUser(Username, Password, Firstname, Lastname, City, Country, Email, Interests, QA);
     if (result instanceof Error) {
         console.log(result);
@@ -81,8 +82,8 @@ app.post("/user/updateUser", async function (req, res) {
 });
 
 app.post("/passwordRetrieval", async function (req, res) {
-    let Username = req.body.Username;
-    let QA = req.body.QA;
+    let Username = req.query.Username;
+    let QA = req.query.QA;
     var result = await user_module.passwordRetrival(Username, QA);
     if (result instanceof Error) {
         console.log(result);
@@ -94,7 +95,7 @@ app.post("/passwordRetrieval", async function (req, res) {
 
 app.post("/user/getFullUserDetails", async function (req, res) {
     let username = req.decoded.username;
-    let user_details = req.body.username_details;
+    let user_details = req.query.username_details;
     if (username !== "admin")
         res.status(401).send("You are not authorized to make that action");
     var result = await user_module.getFullUserDetails(user_details);
@@ -107,7 +108,7 @@ app.post("/user/getFullUserDetails", async function (req, res) {
 });
 
 app.get("/user/getUserDetails", async function (req, res) {
-    let user_details = req.body.username_details;
+    let user_details = req.query.username_details;
     var result = await user_module.getUserDetails(user_details);
     if (result instanceof Error) {
         console.log(result);
@@ -119,8 +120,8 @@ app.get("/user/getUserDetails", async function (req, res) {
 
 
 app.get("/getPopularRandomPoints", async function (req, res) {
-    let amountOfPoints = req.body.pointsAmount;
-    let minRate = req.body.minRate;
+    let amountOfPoints = req.query.pointsAmount;
+    let minRate = req.query.minRate;
     var result = await poi_module.getPopularRandomPoints(amountOfPoints, minRate);
     if (result instanceof Error) {
         console.log(result);
@@ -131,9 +132,9 @@ app.get("/getPopularRandomPoints", async function (req, res) {
 });
 
 app.put("/user/writePointReview", async function (req, res) {
-    let point_id = req.body.point_id;
-    let stars = req.body.stars;
-    let review_txt = req.body.review_txt;
+    let point_id = req.query.point_id;
+    let stars = req.query.stars;
+    let review_txt = req.query.review_txt;
     let username = req.decoded.username;
     var result = await poi_module.writePointReview(username, point_id, stars, review_txt);
     if (result instanceof Error) {
@@ -146,7 +147,7 @@ app.put("/user/writePointReview", async function (req, res) {
 
 
 app.get("/getClickedPointDetails", async function (req, res) {
-    let point_id = req.body.point_id;
+    let point_id = req.query.point_id;
     var result = await poi_module.getClickedPointDetails(point_id);
     if (result instanceof Error) {
         console.log(result);
@@ -157,7 +158,7 @@ app.get("/getClickedPointDetails", async function (req, res) {
 });
 
 app.get("/getMostPopularPoints", async function (req, res) {
-    let categories_name = req.body.categories_name;
+    let categories_name = req.query.categories_name;
     var result = await poi_module.getMostPopularPoints(categories_name);
     if (result instanceof Error) {
         console.log(result);
@@ -168,7 +169,7 @@ app.get("/getMostPopularPoints", async function (req, res) {
 });
 
 app.get("/getPointByName", async function (req, res) {
-    let point_name = req.body.point_name;
+    let point_name = req.query.point_name;
     var result = await poi_module.getPointByName(point_name);
     if (result instanceof Error) {
         console.log(result);
@@ -189,7 +190,7 @@ app.get("/getAllPoints", async function (req, res) {
 });
 
 app.get("/getPointsByCategory", async function (req, res) {
-    let category = req.body.category;
+    let category = req.query.category;
     var result = await poi_module.getPointsByCategory(category);
     if (result instanceof Error) {
         console.log(result);
@@ -223,7 +224,7 @@ app.get("/user/getUserSavedPoints", async function (req, res) {
 
 app.put("/user/saveUserPoints", async function (req, res) {
     let username = req.decoded.username;
-    let userPoints = req.body.user_points;
+    let userPoints = req.query.user_points;
     var result = await poi_module.saveUserPoints(username, userPoints);
     if (result instanceof Error) {
         console.log(result);
@@ -234,7 +235,7 @@ app.put("/user/saveUserPoints", async function (req, res) {
 });
 
 app.get("/getInterestPointReviews", async function (req, res) {
-    let point_id = req.body.point_id;
+    let point_id = req.query.point_id;
     var result = await poi_module.getInterestPointReviews(point_id);
     if (result instanceof Error) {
         console.log(result);
@@ -245,7 +246,7 @@ app.get("/getInterestPointReviews", async function (req, res) {
 });
 
 app.get("/getPointByID", async function (req, res) {
-    let point_id = req.body.point_id;
+    let point_id = req.query.point_id;
     var result = await poi_module.getPointByID(point_id);
     if (result instanceof Error) {
         console.log(result);
@@ -260,7 +261,7 @@ app.delete("/user/deletePoint", async function (req, res) {
     if (username !== "admin")
         res.status(401).send("You are not authorized to make that action");
     else{
-        let point_id = req.body.point_id;
+        let point_id = req.query.point_id;
         var result = await poi_module.deletePoint(point_id);
         if (result instanceof Error) {
             console.log(result);
@@ -276,7 +277,7 @@ app.delete("/user/deleteUser", async function (req, res) {
     if (username !== "admin")
         res.status(401).send("You are not authorized to make that action");
     else{
-        let username_delete = req.body.username_delete;
+        let username_delete = req.query.username_delete;
         var result = await poi_module.deleteUser(username_delete);
         if (result instanceof Error) {
             console.log(result);
@@ -292,12 +293,12 @@ app.put("/user/updatePoint", async function (req, res) {
     if (username !== "admin")
         res.status(401).send("You are not authorized to make that action");
     else{
-        let point_id = req.body.point_id;
-        let point_name = req.body.point_name;
-        let category = req.body.category;
-        let location = req.body.locatio;
-        let description = req.body.descrip;
-        let img_src = req.body.img_src;
+        let point_id = req.query.point_id;
+        let point_name = req.query.point_name;
+        let category = req.query.category;
+        let location = req.query.locatio;
+        let description = req.query.descrip;
+        let img_src = req.query.img_src;
         var result = await poi_module.updatePoint(point_id, point_name, category, location, description, img_src);
         if (result instanceof Error) {
             console.log(result);
@@ -313,12 +314,12 @@ app.put("/user/createPoint", async function (req, res) {
     if (username !== "admin")
         res.status(401).send("You are not authorized to make that action");
     else{
-        let point_id = req.body.point_id;
-        let point_name = req.body.point_name;
-        let category = req.body.category;
-        let location = req.body.location;
-        let description = req.body.description;
-        let img_src = req.body.img_src;
+        let point_id = req.query.point_id;
+        let point_name = req.query.point_name;
+        let category = req.query.category;
+        let location = req.query.location;
+        let description = req.query.description;
+        let img_src = req.query.img_src;
         var result = await poi_module.createPoint(point_name, category, location, description, img_src);
         if (result instanceof Error) {
             console.log(result);
@@ -336,7 +337,7 @@ app.put("/user/createPoint", async function (req, res) {
 //     if (username !== "admin")
 //         res.status(401).send("You are not authorized to make that action");
 //     else{
-//         let countries_xml_src = req.body.countries_xml_src;
+//         let countries_xml_src = req.query.countries_xml_src;
 //         var x = new XMLHttpRequest();
 //         x.open("GET", 'https://www.dropbox.com/s/rh4dct91u8vj74x/countries.xml', true);
 //         x.onreadystatechange = async function () {
